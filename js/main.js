1e700661f69c43a425de6aa519a5e4f1216820ba
seeward <seeward@gmail.com>
2014-12-15 14:27:35 +0200

    added save()

--- js/main.js
+++ js/main.js
@@ -1,42 +1,97 @@
 $(document).ready(function() {
 
-        function init() {
-        	$("#msg").hide();
-        	if(window.localStorage.getItem("saved")){
-        		data = window.localStorage.getItem("saved");
-                $("#txtBody").val(data);
+var currentNote = "defaultNoteName";
 
-               
-        	}
-            
-            };
+    function init() {
+        $("#msg").hide();
+        if (window.localStorage.getItem("saved")) {
+            data = window.localStorage.getItem("saved");
+            $("#txtBody").val(data.note);
 
-            $("#remember").click(function(){
-            	savedTxt = document.getElementById("txtBody").value;
-                        window.localStorage.setItem("saved", savedTxt);
-                        $("#msg").show();
-                        $("#msg").html("Note contents saved.");
-                        setTimeout(function(){
-                        	$("#msg").hide();
-                        }, 1000);
-            });
-            $("#saver").click(function() {
-                txtBody = document.getElementById("txtBody").value;
-                //alert(txtBody);
-                var blob = new Blob([txtBody], {
-                    type: "text/plain;charset=utf-8"
-                });
-                 var name = prompt("Please enter filename");
-    if (name != null) {
-         saveAs(blob, name);
-    }
-               
-            });
 
-            $("#clear").click(function(){
-            	$("#txtBody").val("");
-            	window.localStorage.removeItem("saved");
-            });
-init();
-          
+        }
+
+        getSaved();
+
+    };
+
+
+    function getSaved() {
+
+        $("#noteList").html("");
+        keys = Object.keys(localStorage);
+        console.log(keys);
+        $.each(keys, function(i, obj) {
+            $("#noteList").append("<a class='noteRow btn btn-block btn-custom' id='" + obj + "'>" + obj + "</a>");
         });
+        $("#noteList").show();
+    };
+
+    $("#noteList").on("click", "a", function(e){
+        note = $(this).text();
+        newNote = JSON.parse(window.localStorage.getItem(note));
+
+        msgTxt = newNote.note;
+        currentNote = note;
+        $("#txtBody").val(msgTxt);
+
+    });
+
+    $("#remember").click(function() {
+        $("#noteList").hide();
+        data = {};
+        savedTxt = document.getElementById("txtBody").value;
+        data.note = savedTxt;
+        data.key = prompt("Name this Note:");
+        if (data.key != null) {
+            window.localStorage.setItem(data.key, JSON.stringify(data));
+        }
+
+        $("#msg").show();
+        $("#msg").html("Note contents saved.");
+        setTimeout(function() {
+            $("#msg").hide();
+
+            getSaved();
+        }, 1000);
+    });
+
+
+
+
+
+
+
+
+
+    $("#saver").click(function() {
+        txtBody = document.getElementById("txtBody").value;
+        //alert(txtBody);
+        var blob = new Blob([txtBody], {
+            type: "text/plain;charset=utf-8"
+        });
+        var name = prompt("Please enter filename");
+        if (name != null) {
+            saveAs(blob, name);
+        }
+
+    });
+
+    $("#new").click(function(){
+        $("#txtBody").val("");
+        currentNote = "defaultNoteName";
+    });
+
+
+
+    $("#clear").click(function() {
+        $("#txtBody").val("");
+
+        if(currentNote != "defaultNoteName"){
+            window.localStorage.removeItem(currentNote);
+        }
+        getSaved();
+    });
+    init();
+
+});
